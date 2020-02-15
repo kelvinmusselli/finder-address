19143066889677ecc50b7ec5e351c6db6af86d7a
Kelvin Musselli <kelvinmusselli22@gmail.com>
2020-02-15 08:40:04 -0300

    create-project(start) - creating project

---
+++ build/precache-manifest.20a0cc4aef1d50f256026221a75678ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65629d9e6da0dcc4f1b858ab95cf913c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb3e6981704a178e139a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e96d2eb3b0b57dc33ab7",
+    "url": "/static/js/2.044ebf03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.044ebf03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3e6981704a178e139a",
+    "url": "/static/js/main.e3a4b24c.chunk.js"
+  },
+  {
+    "revision": "9a8564ac8f737eb11b0a",
+    "url": "/static/js/runtime-main.e29ce4f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [] = useState([]);
+
+  return (
+    <>
+      <ul>
+        <li>Luiza Labs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
